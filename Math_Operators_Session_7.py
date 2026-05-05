b53c001cd3551e676c72c69715acbcc2428259aa
Amiya Sinha <writeamiya@gmail.com>
2026-05-05 19:10:34 -0400

    first commit
    	new file:   Math_Operators_Session_7.py
    	new file:   README.md
    	.vscode/

---
+++ Math_Operators_Session_7.py
@@ -0,0 +1,11 @@
+add_var = 10
+print("initial Value of add_var", add_var)
+add_var += 2**5
+print("Value of add_var after addition", add_var)
+modulo_var = 25 % 7
+print("Value of modulo_var after modulo operation", modulo_var)
+floor_division_var = add_var // modulo_var
+print("Value of floor_division_var after floor division operation", floor_division_var)
+# print(add_var)
+print(f"Final Values of add_var = {add_var}\nfinal value of modulo operation = {modulo_var}")
+print(f"final value of floor division operation = {floor_division_var}\n")
